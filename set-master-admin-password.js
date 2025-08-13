a262944388779d066c890f37b7c357098c2dbd0c
Mindsetofchampions <132849068+Mindsetofchampions@users.noreply.github.com>
2025-08-12 23:13:47 -0400

    Updated set-master-admin-password.js

--- set-master-admin-password.js
+++ set-master-admin-password.js
@@ -1,8 +1,8 @@
 // set-master-admin-password.ts
 import { createClient } from '@supabase/supabase-js';
 
-const VITE_SUPABASE_URL='https://cpfcnauiuceialwdbzms.supabase.co'
-const VITE_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc'3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwZmNuYXVpdWNlaWFsd2Riem1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2NTc5NDAsImV4cCI6MjA2OTIzMzk0MH0.NnE8X9Din3b-ZQMSDvap_r9FJdhn2q7sgQAhc93-vwQ
+const VITE_SUPABASE_URL="https://cpfcnauiuceialwdbzms.supabase.co"
+const VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwZmNuYXVpdWNlaWFsd2Riem1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2NTc5NDAsImV4cCI6MjA2OTIzMzk0MH0.NnE8X9Din3b-ZQMSDvap_r9FJdhn2q7sgQAhc93-vwQ"
 
 const supabase = createClient(VITE_SUPABASE_URL!, VITE_SUPABASE_ANON_KEY!);
 
