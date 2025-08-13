8ea78cfcbab545f949f3f6a75aa684481c91f526
Mindsetofchampions <132849068+Mindsetofchampions@users.noreply.github.com>
2025-08-13 00:49:23 -0400

    Updated reset-passwords.js

--- reset-passwords.js
+++ reset-passwords.js
@@ -1,7 +1,11 @@
 // set-master-admin-password.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const SUPABASE_URL = "https://cpfcnauiuceialwdbzms.supabase.co"
+
+const SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwZmNuYXVpdWNlaWFsd2Riem1zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzY1Nzk0MCwiZXhwIjoyMDY5MjMzOTQwfQ.XXuOInGkGEx4W3vW5I92o5sVqdBbAuu7mKCDUHmtXv"
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 /*await supabase.auth.admin.updateUserById(
   '2291731a-6c86-4b1b-afe5-d70fac981481', // masteradmin UUID
