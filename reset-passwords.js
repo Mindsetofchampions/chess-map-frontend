26d4fce29efa6c76f5b4cfb124cb95e4a79cb12a
Mindsetofchampions <132849068+Mindsetofchampions@users.noreply.github.com>
2025-08-13 00:44:36 -0400

    Updated reset-passwords.js

--- reset-passwords.js
+++ reset-passwords.js
@@ -12,6 +12,14 @@ const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SE
 });*/
 
 await supabase.auth.admin.updateUserById(
+  'bfa11af8-0715-48f2-b3da-5371493f398b', // org UUID
+  { password: 'Temp!Admin#2025' }         // pick a strong temp password
+).then(({ data, error }) => {
+  if (error) throw error;
+  console.log('Password updated for', data.user?.email);
+});
+
+await supabase.auth.admin.updateUserById(
   'ff5f9115-2ac7-43da-bab0-e8b704295a2a', // student UUID
   { password: 'Temp!Admin#2025' }         // pick a strong temp password
 ).then(({ data, error }) => {
@@ -19,4 +27,3 @@ await supabase.auth.admin.updateUserById(
   console.log('Password updated for', data.user?.email);
 });
 
-ff5f9115-2ac7-43da-bab0-e8b704295a2a
